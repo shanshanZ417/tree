f9f5a8cfd3915d04fbaa2977386f4a3383d19629
Shanshan Zhang <sz438@cornell.com>
2016-10-03 18:09:51 +0000

    firsttry

--- pytree.py
+++ pytree.py
@@ -1,11 +1,72 @@
 #!/usr/bin/env python3
-import subprocess
-import sys
+import subprocess,sys,os,string
+class XXTree:
+		def __init__(self): 		
+				pass
 
+		def printHelp(self, cmd): 		
+			print 'Please use the following cmd:' 		
+			print '    ' + cmd + ' dir' 		
+			print 'e.g.' 		
+			print '    ' + cmd + ' /home/fkong/tmp'
+		
+		def getTree(self, dir, op): 		
+				list = self.getList(dir, 0, op) 		
+				treelist = [] 	
+				for i in range(0, len(list)):
+						fullpath = list[i] 
+						parpath = os.path.dirname(list[i])
+						filename = os.path.basename(list[i])
+						if(fullpath == dir):
+								treelist.append(fullpath) 
+								continue
 
-# YOUR CODE GOES here
-
-
+						path = fullpath.replace(dir, "")
+						names = path.split("/")
+						name = "`---" + names[len(names) - 1]
+						for j in range(1, len(names) - 1):
+								name = "    " + name
+						treelist.append(name)
+						
+						pos = name.index("`")
+						j = i - 1
+						while j > 0:
+								name = treelist[j]
+								if(name[pos] == '`' or name[pos] == ' '):
+									name = name[0: pos] + "|" + name[pos + 1: len(name)] 	
+									treelist[j] = name 	
+								else:
+										break
+								j = j - 1
+				for i in range(0, len(treelist)):
+						print treelist[i]
+		def getList(self, dir, layer, op):
+				list = []
+				if layer == 0: list.append(dir)
+				files = os.listdir(dir)
+				for file in files:
+						file = os.path.join(dir, file)
+						if os.path.isdir(file):
+								list.append(file)
+								list += self.getList(file, layer + 1, op)
+						elif op == '-d':
+								pass
+						else:
+								list.append(file)
+				return list
+if len(sys.argv) < 2 : 
+		t = XXTree()
+		t.printHelp(sys.argv[0])
+else: 	
+		t = XXTree()
+		dir = None
+		if len(sys.argv) == 2:
+				dir = sys.argv[1]  
+		op = None 
+		if len(sys.argv) == 3:
+				op = sys.argv[1]
+				dir = sys.argv[2]
+		t.getTree(dir, op)
 if __name__ == '__main__':
     # just for demo
     subprocess.run(['tree'] + sys.argv[1:])
