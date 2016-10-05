0e2d517b6353a16b30df0205e7a762c639568701
Shanshan Zhang <sz438@cornell.com>
2016-10-05 04:50:04 +0000

    second try

--- pytree.py
+++ pytree.py
@@ -1,72 +1,73 @@
 #!/usr/bin/env python3
-import subprocess,sys,os,string
+import subprocess
+import sys
+import os
+import string
 class XXTree:
 		def __init__(self): 		
 				pass
 
-		def printHelp(self, cmd): 		
-			print 'Please use the following cmd:' 		
-			print '    ' + cmd + ' dir' 		
-			print 'e.g.' 		
-			print '    ' + cmd + ' /home/fkong/tmp'
-		
-		def getTree(self, dir, op): 		
-				list = self.getList(dir, 0, op) 		
+		def getTree(self, dir): 		
+				directNum = 0
+				filesNum = 0
+				judge = 0
+				list = self.getList(dir, 0)
 				treelist = [] 	
 				for i in range(0, len(list)):
+						final = 0
+						judge = judge + 1
 						fullpath = list[i] 
 						parpath = os.path.dirname(list[i])
 						filename = os.path.basename(list[i])
 						if(fullpath == dir):
 								treelist.append(fullpath) 
 								continue
-
+						if os.path.isdir(fullpath):
+								directNum = directNum + 1
+						else:
+								filesNum = filesNum + 1
+								temp = fullpath.replace(dir, "")
+								temp2 = temp.split("/")
+								if(len(temp2)==2):
+										pass
+								else:
+										final = 1
 						path = fullpath.replace(dir, "")
 						names = path.split("/")
-						name = "`---" + names[len(names) - 1]
-						for j in range(1, len(names) - 1):
-								name = "    " + name
-						treelist.append(name)
-						
-						pos = name.index("`")
-						j = i - 1
-						while j > 0:
-								name = treelist[j]
-								if(name[pos] == '`' or name[pos] == ' '):
-									name = name[0: pos] + "|" + name[pos + 1: len(name)] 	
-									treelist[j] = name 	
+						if(judge != len(list)):
+								if (final == 0):
+										name = "├-- " + names[len(names) - 1]
 								else:
-										break
-								j = j - 1
+										name = "└" + names[len(names)-1]
+								for j in range(1, len(names) - 1):
+										name = "|    " + name						
+								treelist.append(name)
+						else:
+								name = "└" + names[len(names)-1]
+								treelist.append(name)
 				for i in range(0, len(treelist)):
-						print treelist[i]
-		def getList(self, dir, layer, op):
+						print (treelist[i])
+				print("")
+				print(directNum,"directories,",filesNum,"files")
+		def getList(self, dir, layer):
 				list = []
-				if layer == 0: list.append(dir)
+				if layer == 0: 
+						list.append(dir)
 				files = os.listdir(dir)
 				for file in files:
-						file = os.path.join(dir, file)
-						if os.path.isdir(file):
-								list.append(file)
-								list += self.getList(file, layer + 1, op)
-						elif op == '-d':
-								pass
-						else:
-								list.append(file)
+						if not file.startswith("."):
+								file = os.path.join(dir, file)
+								if os.path.isdir(file):
+										list.append(file)
+										list += self.getList(file, layer + 1)
+								else:
+										list.append(file)
 				return list
-if len(sys.argv) < 2 : 
-		t = XXTree()
-		t.printHelp(sys.argv[0])
-else: 	
+if __name__ == '__main__':
 		t = XXTree()
 		dir = None
 		if len(sys.argv) == 2:
-				dir = sys.argv[1]  
-		op = None 
-		if len(sys.argv) == 3:
-				op = sys.argv[1]
-				dir = sys.argv[2]
-		t.getTree(dir, op)
-if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+				dir = sys.argv[1] 
+		if len(sys.argv) == 1:
+				dir = "."
+		t.getTree(dir)
