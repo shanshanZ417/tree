dedd2294eab71117901ecc7528f1db75d62a5703
shanshanZ417 <sz438@cornell.edu>
2016-10-08 01:49:09 -0400

    Update pytree.py
    sytax error

--- pytree.py
+++ pytree.py
@@ -16,58 +16,57 @@ def sortedTree(dir):
 
 
 def buildTree(dir,front):
-	itemOrder = 0
-	list = []
-	global names
-	global directNum
-	global fileNum
-	symbol1 = "    "
-	symbol2 = "│   "
-	files = sortedTree(dir)
-	for path in sorted(files,key=str.lower):
-		curDir = dir + "/" + path
-		itemOrder = itemOrder + 1
-		if os.path.isdir(curDir):
-			directNum = directNum + 1
-			if(itemOrder != len(files)):
-				name = front + "├── "+ path
-				names.append(name)
-				buildTree(curDir,front + symbol2)
-			else:
-				name = front + "└── " + path
-				names.append(name)
-				buildTree(curDir,front + symbol1)
-		else:
-			fileNum = fileNum + 1
-			if (itemOrder != len(files)):
-				name = front + "├── "+ path
-				names.append(name)
-			else:
-				name = front + "└── " + path
-				names.append(name)
+    itemOrder = 0
+    list = []
+    global names
+    global directNum
+    global fileNum
+    symbol1 = "    "
+    symbol2 = "│   "
+    files = sortedTree(dir)
+    for path in sorted(files,key=str.lower):
+        curDir = dir + "/" + path
+        itemOrder = itemOrder + 1
+        if os.path.isdir(curDir):
+            directNum = directNum + 1
+            if(itemOrder != len(files)):
+                name = front + "├── "+ path
+                names.append(name)
+                buildTree(curDir,front + symbol2)
+            else:
+                name = front + "└── " + path
+                names.append(name)
+                buildTree(curDir,front + symbol1)
+        else:
+            fileNum = fileNum + 1
+            if (itemOrder != len(files)):
+                name = front + "├── "+ path
+                names.append(name)
+            else:
+                name = front + "└── " + path
+                names.append(name)
 if __name__ == '__main__':
-	dir = None
-	if (len(sys.argv)!=1 and len(sys.argv)!=2):
-		print("Your enter is not valid!")
-	else:
-		
-		if len(sys.argv) == 2:
-			dir = sys.argv[1]
-		if len(sys.argv) == 1:
-			dir = "."
-		buildTree(dir,"")
-		print(dir)
-		for i in range(0,len(names)):
-			print(names[i])
-		print("")
-		directories = "directories"
-		files = "files"
-		if(directNum == 1):
-			directories = "directory"
-		else:
-			pass
-		if(files == 1):
-			files = "file"
-		else:
-			pass
-		print(str(directNum) + " " + directories + ", " + str(fileNum) + " " + files) 		
+    dir = None
+    if (len(sys.argv)!=1 and len(sys.argv)!=2):
+        print("Your enter is not valid!")
+    else:
+        if len(sys.argv) == 2:
+            dir = sys.argv[1]
+        if len(sys.argv) == 1:
+            dir = "."
+        buildTree(dir,"")
+        print(dir)
+        for i in range(0,len(names)):
+            print(names[i])
+        print("")
+        directories = "directories"
+        files = "files"
+        if(directNum == 1):
+            directories = "directory"
+        else:
+            pass
+        if(files == 1):
+            files = "file"
+        else:
+            pass
+        print(str(directNum) + " " + directories + ", " + str(fileNum) + " " + files) 		
